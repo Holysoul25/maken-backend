5f39c9015e9e60150a179543eea0aa6a3c744f52
holysoul25 <kevinalballay7@gmail.com>
2026-04-18 16:43:58 -0400

    fix port nuevamente

--- index.js
+++ index.js
@@ -220,5 +220,5 @@ app.post('/contacto', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Maken API corriendo en puerto ${PORT}`));
